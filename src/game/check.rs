0d8d0ab41da339a1e51665c4ae4c37154187f2cb
sofie114 <slushtre@gmail.com>
2025-09-30 21:12:51 +0200

    checkmate fixes

--- src/game/check.rs
+++ src/game/check.rs
@@ -8,7 +8,7 @@ impl game_state {
                     let ui = i as i32; let uu = u as i32;
                     let valid_pos = self.valid_moves(ui, uu);
                     for &(a, b) in &valid_pos {
-                        if self.get_piece(a, b) == "king".to_string() && self.get_player(a, b)!=player {
+                        if self.get_piece(a, b) == "king".to_string() && self.get_player(a, b) != player {
                             return true;
                         }
                     }
@@ -20,24 +20,21 @@ impl game_state {
     
     pub fn is_checkmate(&mut self) -> bool{ // if i am checkmating opponent, assuming i am checking
         // find king, if no move 
-        let mut opponent = 'w';
-        let mut r = 1; let mut c = 1;
+        let opponent = if self.turn=='w' {'b'} else {'w'};
+
         for i in 1..9 {
             for u in 1..9 {
-                if self.board[i][u]=="king".to_string() && self.player[i][u]!=self.turn {
-                    opponent = self.turn;
-                    r = i as i32; c = u as i32; 
-                    break;
+                if self.player[i][u]!=self.turn {
+                    let r = i as i32; let c = u as i32; 
+
+                    let valid_pos = self.valid_moves(r, c);
+                    for &(a, b) in &valid_pos {
+                        let mut upd_game = self.clone();
+                        upd_game.move_piece(r, c, a, b);
+                        if !upd_game.is_checking(opponent) {return false;}
+                    }
                 }
             }
-        }
-
-        let me = self.get_player(r, c); let valid_pos = self.valid_moves(r, c);
-        for &(a, b) in &valid_pos {
-            let piece = self.get_piece(a, b); let player = self.get_player(a, b);
-            let mut upd_game = self.clone();
-            upd_game.move_piece(r, c, a, b);
-            if !upd_game.is_checking(opponent) {return false;}
         }
         return true;
     }
